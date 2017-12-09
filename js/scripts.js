226d018174295923783c327d8390b725d8a51977
mp <michael.pinaud@gmail.com>
2017-12-09 12:03:33 -0800

    add scroll to top fixed bar and stylized show and hide and media query sizes

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(function(){
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 100) {
+			$('.scrollToTop').fadeIn();
+		} else {
+			$('.scrollToTop').fadeOut();
+		}
+	});
+
+	$('.scrollToTop').click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+		return false;
+	});
+});
