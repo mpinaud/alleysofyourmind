5dc718a83ed801d644637f99772f08890d4e078d
mp <michael.pinaud@gmail.com>
2017-12-09 19:53:14 -0800

    add max capacity section to layout and images in folder

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,7 @@ $(function(){
 	});
 
 	$('.scrollToTop').click(function(){
-		$('html, body').animate({scrollTop : 0},800);
+		$('html, body').animate({scrollTop : 0},300);
 		return false;
 	});
 });
